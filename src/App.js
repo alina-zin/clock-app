cf4a4a25cb9d6bf00c7da52ba9d329f2c4fd815f
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-02-04 18:40:45 +0200

    clock rdy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Clock () {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+
+  return <div style={{ margin: '30px' }}>{time.toLocaleTimeString()}</div>
 }
-
-export default App;
